bf29f9a49ed90f9e744e517c9e0bd0598245bc48
Amy <86963506+aeposten@users.noreply.github.com>
2023-02-21 07:22:31 -0800

    added env

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import SavedColors from "./components/SavedColors/SavedColors";
 import FetchedPhotos from "./components/FetchedPhotos/FetchedPhotos";
 import "./App.css";
 
+const KEY = process.env.REACT_APP_API_KEY
 function App() {
   // State
   const [schemeColors, setSchemeColors] = useState([]);
@@ -66,7 +67,7 @@ function App() {
 
   const fetchImages = useCallback(() => {
     try {
-      fetch(`https://api.unsplash.com/search/photos?page=1&per_page=5&query=cat&color=${colorRange}&client_id=QxH3vsdpcwpuz9h3-7HMlZv-hOwQm_Obvqi0EU-Et-A`)
+      fetch(`https://api.unsplash.com/search/photos?page=1&per_page=5&query=cat&color=${colorRange}&client_id=${KEY}`)
         .then((response) => response.json())
         .then((data)=> setFetchedPhotos((prevFetchedPhotos) => data.results))
     } catch (error) {
